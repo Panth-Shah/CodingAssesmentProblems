d367772e88694486f966a3c665dbafe57222ace3
Panth-Shah <panth5shah@gmail.com>
2021-03-07 18:44:32 -0500

    [Microsoft OA] - Min Steps to Make Piles Equal Height solution

---
+++ Min Steps to Make Piles Equal Height/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Min_Steps_to_Make_Piles_Equal_Height
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+
+    internal class Solution
+    {
+
+    }
+}
